38d24fefb7c0f3399dc84d7ab5f54a07802e8147
Manthan Keim <manthankeim7@gmail.com>
2020-04-15 14:53:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,24 @@ addEventListener('fetch', event => {
  */
 
 
+
+class LinkRewriter {
+    element(element) {
+        element.setAttribute('href', 'https://manthankeim.github.io')
+        element.setInnerContent('Manthan`s Portfolio')
+    }
+}
+
+
+class BodyRewriter {
+    element(element) {
+        element.setInnerContent('Just changed the content so that you cant figure out what was here before', { html: true} )
+    }
+}
+
+const REWRITER  = new HTMLRewriter()
+.on('p#description', new BodyRewriter())
+.on('a#url', new LinkRewriter())
     
 async function handleRequest(request) {
     let cache = caches.default
@@ -16,8 +34,8 @@ async function handleRequest(request) {
     var randomValue = Math.random();
     if(randomValue < 0.5){
         const resp2 = await fetch(data['variants'][0]);
-        const data2 = await resp2.body;
-        return new Response(data2)
+//        const data2 = await resp2.body;
+        return new Response(REWRITER.transform(resp2).body)
     }
     else{
         const resp2 = await fetch(data['variants'][1]);
